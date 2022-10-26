e9441910a1b8c70834cbf807a411faa5c1196d6a
Milena Carvalho <nascarvalho.m@gmail.com>
2022-10-26 14:27:02 -0300

    implementando rotas

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common';
-import { UsersService } from './users.service';
-
-@Controller('users')
-export class UsersController {
-    constructor(private readonly usersService: UsersService){}
-}
+import {
+    Body,
+    Controller,
+    Post,
+    Get,
+    Param,
+    ParseUUIDPipe,
+    Patch,
+  } from '@nestjs/common';
+  import { UsersService } from './users.service';
+  import { CreateUserDTO } from './dto/createUser.dto';
+  import { UpdateUserDTO } from './dto/updateUser.dto';
+  
+  @Controller('users')
+  export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+  
+    // criar
+    @Post()
+    create(@Body() req: CreateUserDTO) {
+      return this.usersService.create();
+    }
+    // listar todos  localhost:3000/users
+    @Get()
+    findAll() {
+      return this.usersService.findAll();
+    }
+    // listar um  localhost:3000/users/1
+    @Get(':id')
+    findOne(@Param('id', ParseUUIDPipe) id: number) {
+      return this.usersService.findOne(id);
+    }
+    // atualizar
+    @Patch(':id')
+    update(@Param('id', ParseUUIDPipe) id: number, @Body() req: UpdateUserDTO) {
+      return this.usersService.update(id, req);
+    }
+    // deletar
+  }
